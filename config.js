cf9171d212dff47d826d106a36d82c291f034e8d
pedrolangsakalam2-creator <pedrolangsakalam2@gmail.com>
2026-02-13 15:22:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "Baby i have some questions for you. Are you ready?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
             secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
