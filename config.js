fdcaeb92a8d8e3db2dc9bb2b9acb43f364f2700d
pedrolangsakalam2-creator <pedrolangsakalam2@gmail.com>
2026-02-13 15:59:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/daaxrddkg/video/upload/v1770965639/videoplayback_dlp53r.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/daaxrddkg/video/upload/v1770969568/Paramore_-_mtwgo8.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
