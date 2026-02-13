8e277e478bf9cb3b89c972a0069b44cbb9ae2752
pedrolangsakalam2-creator <pedrolangsakalam2@gmail.com>
2026-02-13 15:34:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -77,7 +77,7 @@ const CONFIG = {
     // Add your own music URL after getting proper licenses
     music: {
         enabled: true,                     // Music feature is enabled
-        autoplay: true,                    // Try to autoplay (note: some browsers may block this)
+        autoplay: false,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/daaxrddkg/video/upload/v1770965639/videoplayback_dlp53r.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
